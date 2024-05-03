8ce810c4ca7bcaf3ed7f77330637e408e704e0fd
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-05-03 12:24:13 +0000

    Tipo de dados

---
+++ Tipos de dados/Tipagem dinamica.py
@@ -0,0 +1,13 @@
+# Atribuindo Valores
+nome = "Marta"
+idade = 20
+peso = 62.500
+
+# Exibindo Dados
+print(nome)
+print(idade)
+print(peso)
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso:.2f}")
---
+++ Tipos de dados/Tipagem estatica.py
@@ -0,0 +1,9 @@
+# Atribuindo Valores
+nome: str = "Marta"
+idade: int = 20
+peso: float = 62.500
+
+#Exibindo Resultados
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso:.2f}")
